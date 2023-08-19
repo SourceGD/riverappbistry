f72f632b3bb52ac3a35e2a530ac0a4d6d9ae3828
Arnaud Meunier <arnaudmeunier02@gmail.com>
2023-08-19 20:09:49 +0200

    []

--- definitions.py
+++ definitions.py
@@ -2,3 +2,46 @@ from os import path
 
 ROOT_DIR = path.dirname(path.abspath(__file__))
 CONFIG_DIR = path.join(ROOT_DIR, 'config')
+PROJECTS_DIR = path.join(ROOT_DIR, "projects")
+
+PROJECT_DEFAULT_STRUCT = {
+    "project_name": "",
+    "last_step_done":"",
+    
+    "video_configuration": {
+        "video" : "",
+        "start_time": 0,
+        "end_time": 1
+    },
+
+    "bathymetry" : {
+        "x": [],
+        "y": []
+    },
+
+    "beacons" : {
+        "points": [],
+        "p1_to_p2" : 0,
+        "p2_to_p3": 0,
+        "p3_to_p4": 0,
+        "p4_to_p1": 0,
+        "p1_to_p3": 0,
+        "p2_to_p4": 0
+    },
+
+    "piv": {
+        "filters": [],
+        "piv_file_data": ""
+    },
+
+    "post_process": {
+        "masks": []
+    },
+
+    "transect": {
+        "points": [],
+        "transect_file_data": ""
+    }
+}
+
+PROJECT_STEPS = ["video_configuration", "bathymetry", "beacons", "piv"]
