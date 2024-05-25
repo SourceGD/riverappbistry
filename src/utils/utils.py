16d113e6c6c504ba76e9bd0dba5df406f4991779
andreas <abombaert@hotmail.com>
2024-05-25 20:14:47 +0200

    commented check-internet to force local use

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,11 +2,12 @@ import requests
 
 
 def check_internet():
-    url = "http://www.google.com"
-    timeout = 5
-    try:
-        _ = requests.get(url, timeout=timeout)
-        return True
-    except requests.ConnectionError:
-        print("No internet connection available.")
+    # TODO uncomment this code if you want to use the API version
+    # url = "http://www.google.com"
+    # timeout = 5
+    # try:
+    #     _ = requests.get(url, timeout=timeout)
+    #     return True
+    # except requests.ConnectionError:
+    #     print("No internet connection available.")
     return False
